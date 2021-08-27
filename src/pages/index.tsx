7481f9b1bbc0bfd625a30945e3566d088027d579
Blake Wilson <blake@blake.id>
2021-08-27 11:13:15 -0500

    Get map component setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import GoogleMapReact from "google-map-react";
+
 export default function Page() {
-  return <div>Home page</div>;
+  return (
+    <div style={{ height: `100vh`, width: `100%` }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY }}
+        defaultCenter={{ lat: 30.2672, lng: -97.7393 }}
+        defaultZoom={10}
+      />
+    </div>
+  );
 }
