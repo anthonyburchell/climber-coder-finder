4a5ae25851e2e4bed60c649bcceaee7ae48c49c4
Francis Agulto <francis.agulto@wpengine.com>
2021-09-15 12:21:35 -0500

    initial changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
+import { getNextStaticProps } from "@faustjs/next";
 import GoogleMapReact from "google-map-react";
+import { GetStaticPropsContext } from "next";
 import React from "react";
 import Card from "../../components/Card/Card";
 import Map from "../../components/map";
+import { client } from "../client";
+
 export default function Page() {
   return (
     <>
       <Map />
     </>
   );
+}
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page: Page,
+    client,
+  });
 }
